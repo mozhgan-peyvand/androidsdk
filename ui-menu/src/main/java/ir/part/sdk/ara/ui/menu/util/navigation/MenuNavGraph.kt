ca776e61b303d9665cb0df726c90ad194b3ee241
morteza.moghadam <morteza.moghadam>
2022-08-03 11:42:26 +0430

    Improve main menu (#38)
    * Move divider fun in MenuItem
    * Add todo
    * Navigate to change password screen with menu router
    * Change 'Change Password path' item in MenuRouter
    * Remove dependency of ui-menu to ui-user module

--- ui-menu/src/main/java/ir/part/sdk/ara/ui/menu/util/navigation/MenuNavGraph.kt
+++ ui-menu/src/main/java/ir/part/sdk/ara/ui/menu/util/navigation/MenuNavGraph.kt
@@ -4,7 +4,7 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.navigation
 import ir.part.sdk.ara.ui.menu.screens.MenuScreen
-import ir.part.sdk.ara.ui.user.util.navigation.navigateToChangePasswordScreen
+
 
 fun NavGraphBuilder.addMenuGraph(navController: NavHostController) {
     navigation(
